f641cd47c917318075e708639352f61d0fdd6d60
Chris Williams <cw14910@nuuk.ggy.bris.ac.uk>
2016-04-22 13:03:10 +0100

    fixed indentation and bugs

--- txt_file_manipulation.py
+++ txt_file_manipulation.py
@@ -9,6 +9,7 @@ Various methods for working with txt and csv formatted files
 
 import util
 import pandas as pd
+import numpy as np
 from astropy.time import Time
 
 def resample_csv_file(file_in, file_out, n=2):
@@ -41,21 +42,22 @@ def concat2csv(in_file, out_file):
 
 
 def check_date_range(data, date_column, sep_type="", skip_rows=0, date_format="jd"):
-	"""
-	Reads in a text file containg date information and prints out the min and max years of data present.
+    """
+    Reads in a text file containg date information and prints out the min and max years of data present.
 
-	VARIABLES
+    VARIABLES
 	
-	data 				= a text file
-	date_column         = the column containg time stamp information
-	sep_type			= separator type (defaults to white space delimited)
-	skip_rows           = number of header rows to skip (defaults to zero)
-	date_format		    = date format type (see here for types: http://docs.astropy.org/en/stable/time/#using-astropy-time)
+    data 	 = a text file
+    date_column  = the column containg time stamp information
+    sep_type	 = separator type (defaults to white space delimited)
+    skip_rows    = number of header rows to skip (defaults to zero)
+    date_format	 = date format type (see here for types: http://docs.astropy.org/en/stable/time/#using-astropy-time)
 
-	RETURNS
+    RETURNS
 
-	Nothing
-	"""
+    Nothing
+    """
+
     if sep_type=="":
         info=pd.read_csv(data, skiprows=skip_rows, header=None, delim_whitespace=True) 
     else:
@@ -69,107 +71,106 @@ def check_date_range(data, date_column, sep_type="", skip_rows=0, date_format="j
 
 
 def read_header(in_file, num_header_rows, sep_type =""):
-	"""
-	Reads in the header rows of a text file.
+    """
+    Reads in the header rows of a text file.
 
-	VARIABLES
+    VARIABLES
 	
-	in_file				= a text file
-	out_file			= an output file
-	sep_type			= separator type (defaults to white space delimited)
-	skip_rows           = number of header rows to skip (defaults to zero)
+    in_file	= a text file
+    out_file	= an output file
+    sep_type	= separator type (defaults to white space delimited)
+    skip_rows   = number of header rows to skip (defaults to zero)
 
-	RETURNS
+    RETURNS
 
-	header (pandas dataframe object)
+    header (pandas dataframe object)
+    """
 
-	"""
-
-	if sep_type=="":
-	    header=pd.read_csv(in_file,delim_whitespace=True, header = None, chunksize=1).read(num_header_rows)
-	else:
-	    header=pd.read_csv(in_file, sep=sep_type, header = None, chunksize=1).read(num_header_rows)
-
-	return header
+    if sep_type=="":
+        header=pd.read_csv(in_file,delim_whitespace=True, header = None, chunksize=1).read(num_header_rows)
+    else:
+        header=pd.read_csv(in_file, sep=sep_type, header = None, chunksize=1).read(num_header_rows)
+    
+    return header
 
 
 def check_duplicates(in_file, sep_type="", header_rows=0):
-	"""
-	Checks for duplicated rows in a given file.
+    """
+    Checks for duplicated rows in a given file.
+
+    VARIABLES
+    in_file	= a text file
+    sep_type	= separator type (defaults to white space delimited)
+    header_rows = number of header rows to skip (defaults to zero i.e. no header)
 	
-	VARIABLES
-	in_file				= a text file
-	sep_type			= separator type (defaults to white space delimited)
-	header_rows         = number of header rows to skip (defaults to zero i.e. no header)
-	
-	RETURNS
+    RETURNS
 
-	Nothing
-	"""
+    Nothing
+    """
 
-	if sep_type=="":
-	    data=pd.read_csv(in_file, skiprows=header_rows, header=None, delim_whitespace=True) 
-	else:
-	    data=pd.read_csv(in_file, skiprows=header_rows, header=None, sep=sep_type)
+    if sep_type=="":
+        data=pd.read_csv(in_file, skiprows=header_rows, header=None, delim_whitespace=True) 
+    else:
+        data=pd.read_csv(in_file, skiprows=header_rows, header=None, sep=sep_type)
 
-	dup=data.duplicated(keep='first')
-	dup_TRUE=np.where(dup==TRUE)
-	len_dup_TRUE_indx=len(dup_TRUE[0])
+    dup=data.duplicated(keep='first')
+    dup_True=np.where(dup==True)
+    len_dup_True_indx=len(dup_True[0])
 
-	if len_dup_False_indx == 0:
-		print("No duplicated rows in %s" %(in_file))
-	else:	
-		print("%i duplicated rows found in %s" %(len_dup_TRUE_indx, in_file))
+    if len_dup_True_indx == 0:
+	print("No duplicated rows in %s" %(in_file))
+    else:	
+	print("%i duplicated rows found in %s" %(len_dup_True_indx, in_file))
 
 
 def strip_duplicates(in_file, out_file, sep_type="", header_rows=0):
-	"""
-	Read in text file and output new version of the file without duplicates (retaining only the first instance).
+    """
+    Read in text file and output new version of the file without duplicates (retaining only the first instance).
 
-	VARIABLES
+    VARIABLES
 
-	in_file				= a text file
-	out_file			= an output file
-	sep_type			= separator type (defaults to white space delimited)
-	header_rows         = number of header rows to skip (defaults to zero i.e. no header)
+    in_file	  = a text file
+    out_file	  = an output file
+    sep_type      = separator type (defaults to white space delimited)
+    header_rows   = number of header rows to skip (defaults to zero i.e. no header)
 	
-	RETURNS
+    RETURNS
 
-	Nothing
-	"""
+    Nothing
+    """
 
-	util.check_output_dir(out_file)
+    util.check_output_dir(out_file)
 
-	if header_rows !=0: header=read_header(in_file, num_header_rows=header_rows, sep_type ="")
+    if header_rows !=0: header=read_header(in_file, num_header_rows=header_rows, sep_type ="")
 
-	if sep_type=="":
-	    data=pd.read_csv(in_file, skiprows=header_rows, header=None, delim_whitespace=True) 
-	else:
-	    data=pd.read_csv(in_file, skiprows=header_rows, header=None, sep=sep_type)
+    if sep_type=="":
+        data=pd.read_csv(in_file, skiprows=header_rows, header=None, delim_whitespace=True) 
+    else:
+        data=pd.read_csv(in_file, skiprows=header_rows, header=None, sep=sep_type)
 
-	dup=data.duplicated(keep='first')
-	dup_False=np.where(dup==False)
+    dup=data.duplicated(keep='first')
+    dup_False=np.where(dup==False)
 	
-	no_dup=data.loc[dup_False]
+    no_dup=data.loc[dup_False]
 
-	len_no_dup=no_dup.shape[0]
-	len_dup_False_indx=len(dup_False[0])
+    len_no_dup=no_dup.shape[0]
+    len_dup_False_indx=len(dup_False[0])
 
-	try:
-		assert len_no_dup == len_dup_indx
-	except AssertionError:
-		print("Removal of duplicates and creation of new output failed.")
-		print("Length of no duplicated indices does not match the subsampled main dataframe... fucntion failiure :(")
+    try:
+        assert len_no_dup == len_dup_indx
+    except AssertionError:
+  	print("Removal of duplicates and creation of new output failed.")
+	print("Length of no duplicated indices does not match the subsampled main dataframe... fucntion failiure :(")
 
 	
-	if header_rows !=0: 
-		frames = [header, no_dup]
-		no_dup = pd.concat(frames)
+    if header_rows !=0: 
+ 	frames = [header, no_dup]
+	no_dup = pd.concat(frames)
 
-	if sep_type=="":
-		no_dup.to_csv(out_file, sep="\t", header=False, index=False)
-	else:
-		no_dup.to_csv(out_file, sep=sep_type, header=False, index=False)
+    if sep_type=="":
+	no_dup.to_csv(out_file, sep="\t", header=False, index=False)
+    else:
+ 	no_dup.to_csv(out_file, sep=sep_type, header=False, index=False)
 
 
 		
