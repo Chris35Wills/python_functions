4cbf1dd93fb053a89f53e74717c350d8bc47ca7f
CN Williams <cw14910@bristol.ac.uk>
2016-05-23 17:56:51 +0100

    additional index search added

--- array_access.py
+++ array_access.py
@@ -2,7 +2,7 @@ from __future__ import division
 
 import numpy as np
 
-def get_index(arr, value):
+def get_first_index(arr, value):
 	"""
 	Gets the first index of a value (or index of an element closest to a given value) from a numpy array
 
@@ -24,3 +24,46 @@ def get_index(arr, value):
 
 	return pos
 
+def get_index_1d(array, select_array):
+	"""
+	Get the index of values from select_array in array
+
+	Variables:
+	array        : 	a 1d numpy array
+	select array : 	a 1d numpy array
+	
+	Return:
+	1d array of indicies
+
+	Example:
+	a=np.array([1,2,3,4,5,6,6,6,6,7,7,8,9,9])
+	b=np.array([6,7,9])
+	In  : get_index_1d(a, b)
+	Out : (array([ 5,  6,  7,  8,  9, 10, 12, 13], dtype=int64),)
+	"""
+	idx = np.in1d(array.ravel(), select_array.ravel())
+	x_indx = np.where(idx.reshape(array.shape))
+	return x_indx
+
+
+def get_index_2d(array, select_array):
+	"""
+	Get the index of values from select_array in array
+
+	Variables:
+	array        : 	a 2d numpy array
+	select array : 	a 2d numpy array
+
+	Return:
+	array of indicies in x
+	array of indicies in y
+
+	Example:
+	a=np.array([1,2,3,4,5,6,6,6,6,7,7,8,9,9],[1,2,3,4,5,6,6,6,6,7,7,8,9,9])
+	b=np.array([6,7,9])
+	In  : get_index_1d(a, b)
+	Out : (array([ 5,  6,  7,  8,  9, 10, 12, 13], dtype=int64),)
+	"""
+	idx = np.in1d(array.ravel(), select_array.ravel())
+	y_indx, x_indx = np.where(idx.reshape(array.shape))
+	return y_indx, x_indx
