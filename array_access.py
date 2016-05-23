1ba6257e4c511a15477f9edc05bf6e34a90966b0
CN Williams <cw14910@bristol.ac.uk>
2016-05-23 17:54:12 +0100

    0 value case added

--- array_access.py
+++ array_access.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import numpy as np
 
 def get_index(arr, value):
@@ -13,9 +15,12 @@ def get_index(arr, value):
 	pos 	: an index
 	"""
 	
-	test= (arr)/np.float(value) 				# want value closest to 1
-	test= np.abs(test-1) 						# want value closest to 1
-	pos = np.int(np.where(test==test.min())[0]) # get index of element closest to distance of interest
+	if value != 0.:
+		test= (arr)/np.float(value) 				# want value closest to 1
+		test= np.abs(test-1) 						# want value closest to 1
+		pos = np.int(np.where(test==test.min())[0]) # get index of element closest to value of interest
+	elif value == 0.:
+		pos = np.int(np.where(arr==value)[0]) 		# get index of element closest to value of interest
 
 	return pos
 
