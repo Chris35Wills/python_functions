7120a5f8b4ed08ee1ee1239b132d933a6825a9c4
CN Williams <cw14910@bristol.ac.uk>
2016-07-06 15:11:00 +0100

    added xy option

--- density_hull_approximator.py
+++ density_hull_approximator.py
@@ -34,7 +34,26 @@ def density_hull_approximator(density_xyz_file, points_xy_file, opath, opath_no_
 	if save_contours_xy==1:
 		concave_hull_funcs.write_contour_verts(opath, contour_x_list, contour_y_list)
 
+def density_hull_approximator_xy(density_xyz_file, points_x, points_y, opath, opath_no_file, itr, density_boundary=-16.500, save_contours_xy=0):
+	
+	xx,yy,zz = concave_hull_funcs.get_density_xyz(density_xyz_file) 
+	resamp_pnt_xx = points_x
+	resamp_pnt_yy = points_y
 
+	Z_log = concave_hull_funcs.grid_log_density(xx,yy,zz) 
+
+	smooth, extent = concave_hull_funcs.smooth_density_surface_points(Z_log, resamp_pnt_xx, resamp_pnt_yy, opath_no_file, itr) ## PLOT GAUSSIAN FILTERED DENSITY SURFACE [WITH POINTS]
+	#smooth, extent = concave_hull_funcs.smooth_density_surface_sans_points(Z_log, resamp_pnt_xx, resamp_pnt_yy)
+
+	#cs = concave_hull_funcs.grid_contour_specified(smooth, extent, density_boundary, resamp_pnt_xx, resamp_pnt_yy)	## SURFACE CONTOUR
+	cs = concave_hull_funcs.grid_contour_specified_input_points(smooth, extent, density_boundary, resamp_pnt_xx, resamp_pnt_yy, opath_no_file, itr)
+	#cs = concave_hull_funcs.grid_contour(smooth, extent, resamp_pnt_xx, resamp_pnt_yy)
+
+	contour_x_list, contour_y_list = concave_hull_funcs.contour_vertices(cs) 	
+	concave_hull_funcs.contour_points_check_plot(smooth, contour_x_list, contour_y_list, density_boundary, extent, opath_no_file, itr)
+
+	if save_contours_xy==1:
+		concave_hull_funcs.write_contour_verts(opath, contour_x_list, contour_y_list)
 
 def density_CONTOURS(density_xyz_file, points_xy_file, opath, opath_no_file, itr, save_contours_xy=0, show=0):
 	'''
