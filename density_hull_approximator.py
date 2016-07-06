b2f17997ada7970012f36ded32b1d8d542d6a990
CN Williams <cw14910@bristol.ac.uk>
2016-07-06 15:24:55 +0100

    added xy option

--- density_hull_approximator.py
+++ density_hull_approximator.py
@@ -14,19 +14,17 @@ output path for images if to be saved and contour xy output files
 density boundary limit i.e. the log of the point density you wish to use as the boundary
 '''
 
-def density_hull_approximator(density_xyz_file, points_xy_file, opath, opath_no_file, itr, density_boundary=-16.500, save_contours_xy=0):
+def density_hull_approximator_xy(density_xyz_file, points_x, points_y, opath, opath_no_file, itr, density_boundary=-16.500, save_contours_xy=0):
 	
 	xx,yy,zz = concave_hull_funcs.get_density_xyz(density_xyz_file) 
-	resamp_pnt_xx, resamp_pnt_yy = concave_hull_funcs.get_points_xy_resamp(points_xy_file) 
+	resamp_pnt_xx = points_x
+	resamp_pnt_yy = points_y
 
 	Z_log = concave_hull_funcs.grid_log_density(xx,yy,zz) 
 
 	smooth, extent = concave_hull_funcs.smooth_density_surface_points(Z_log, resamp_pnt_xx, resamp_pnt_yy, opath_no_file, itr) ## PLOT GAUSSIAN FILTERED DENSITY SURFACE [WITH POINTS]
-	#smooth, extent = concave_hull_funcs.smooth_density_surface_sans_points(Z_log, resamp_pnt_xx, resamp_pnt_yy)
 
-	#cs = concave_hull_funcs.grid_contour_specified(smooth, extent, density_boundary, resamp_pnt_xx, resamp_pnt_yy)	## SURFACE CONTOUR
 	cs = concave_hull_funcs.grid_contour_specified_input_points(smooth, extent, density_boundary, resamp_pnt_xx, resamp_pnt_yy, opath_no_file, itr)
-	#cs = concave_hull_funcs.grid_contour(smooth, extent, resamp_pnt_xx, resamp_pnt_yy)
 
 	contour_x_list, contour_y_list = concave_hull_funcs.contour_vertices(cs) 	
 	concave_hull_funcs.contour_points_check_plot(smooth, contour_x_list, contour_y_list, density_boundary, extent, opath_no_file, itr)
@@ -34,11 +32,28 @@ def density_hull_approximator(density_xyz_file, points_xy_file, opath, opath_no_
 	if save_contours_xy==1:
 		concave_hull_funcs.write_contour_verts(opath, contour_x_list, contour_y_list)
 
-def density_hull_approximator_xy(density_xyz_file, points_x, points_y, opath, opath_no_file, itr, density_boundary=-16.500, save_contours_xy=0):
-	
+
+def density_CONTOURS_xy(density_xyz_file, points_x, points_y, opath, opath_no_file, itr, save_contours_xy=0, show=0):
+	'''
+	Just saves a contoured density image with overlying points - useful to estimate the density_boundary value	
+	'''
 	xx,yy,zz = concave_hull_funcs.get_density_xyz(density_xyz_file) 
 	resamp_pnt_xx = points_x
 	resamp_pnt_yy = points_y
+
+	Z_log = concave_hull_funcs.grid_log_density(xx,yy,zz) 
+
+	smooth, extent = concave_hull_funcs.smooth_density_surface_points(Z_log, resamp_pnt_xx, resamp_pnt_yy, opath_no_file, itr) ## PLOT GAUSSIAN FILTERED DENSITY SURFACE [WITH POINTS]
+	#smooth, extent = concave_hull_funcs.smooth_density_surface_sans_points(Z_log, resamp_pnt_xx, resamp_pnt_yy)
+
+	cs = concave_hull_funcs.grid_contour(smooth, extent, resamp_pnt_xx, resamp_pnt_yy, opath_no_file, itr, show=show)
+
+#######################
+
+def density_hull_approximator(density_xyz_file, points_xy_file, opath, opath_no_file, itr, density_boundary=-16.500, save_contours_xy=0):
+	
+	xx,yy,zz = concave_hull_funcs.get_density_xyz(density_xyz_file) 
+	resamp_pnt_xx, resamp_pnt_yy = concave_hull_funcs.get_points_xy_resamp(points_xy_file) 
 
 	Z_log = concave_hull_funcs.grid_log_density(xx,yy,zz) 
 
@@ -61,21 +76,6 @@ def density_CONTOURS(density_xyz_file, points_xy_file, opath, opath_no_file, itr
 	'''
 	xx,yy,zz = concave_hull_funcs.get_density_xyz(density_xyz_file) 
 	resamp_pnt_xx, resamp_pnt_yy = concave_hull_funcs.get_points_xy_resamp(points_xy_file) 
-
-	Z_log = concave_hull_funcs.grid_log_density(xx,yy,zz) 
-
-	smooth, extent = concave_hull_funcs.smooth_density_surface_points(Z_log, resamp_pnt_xx, resamp_pnt_yy, opath_no_file, itr) ## PLOT GAUSSIAN FILTERED DENSITY SURFACE [WITH POINTS]
-	#smooth, extent = concave_hull_funcs.smooth_density_surface_sans_points(Z_log, resamp_pnt_xx, resamp_pnt_yy)
-
-	cs = concave_hull_funcs.grid_contour(smooth, extent, resamp_pnt_xx, resamp_pnt_yy, opath_no_file, itr, show=show)
-
-def density_CONTOURS_xy(density_xyz_file, points_x, points_y, opath, opath_no_file, itr, save_contours_xy=0, show=0):
-	'''
-	Just saves a contoured density image with overlying points - useful to estimate the density_boundary value	
-	'''
-	xx,yy,zz = concave_hull_funcs.get_density_xyz(density_xyz_file) 
-	resamp_pnt_xx = points_x
-	resamp_pnt_yy = points_y
 
 	Z_log = concave_hull_funcs.grid_log_density(xx,yy,zz) 
 
